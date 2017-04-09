bd86bf5ef11669dbd7042234327ab34d895da686
David Fonollosa <david.fonollosa@innofis.com>
2017-04-09 12:09:49 +0200

    stylying mobile, adding tests

--- src/board/test/board-specs.js
+++ src/board/test/board-specs.js
@@ -1,16 +1,45 @@
 
 import {expect} from 'chai';
-//import board
+import {default as Board} from '../models/board.js';
+import {default as Brush} from '../../brushes/models/brush.js'
+import {default as WidgetBase, 
+        createElement, 
+        addAttributeToElement,
+        buildNode,
+        buildCard,
+        getScreenWidth,
+        getScreenHeight} from '../../common/widgetBase';
 
-describe('Service logic', () => {
+
+describe('Board Component logic', () => {
 
   describe('create new Board instance', () => {
-
     it('call without params', () => {
-      const result = 'ok';
-      expect(result).to.equal('ok');
+      const result = new Board(null, []);
+      expect(result).to.be.an.instanceof(Board);
+    });
+
+    it('call with Container param', () => {
+      const boardContainer = buildNode('div', {})
+      const result = new Board([]);
+      expect(result).to.be.an.instanceof(Board);
     });
 /*
+    it('call with params', () => {
+      const name = 'User';
+      const result = getHello(name);
+
+      expect(result).to.equal(`Hello dear ${name}`);
+    });
+    */
+  });
+
+  describe('validate Board defaults', () => {
+    it('board without Brushes', () => {
+      const result = new Board(null, []);
+      expect(result).to.be.an.instanceof(Board);
+    });
+  /*
     it('call with params', () => {
       const name = 'User';
       const result = getHello(name);
---
+++ src/board/test/toolsPalette-specs.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import {ToolsPalette} from '../models/toolsPalette.js';
+
+describe('Tools Palette Module logic', () => {
+
+  describe('create new ToolsPalette instance', () => {
+
+    it('call without params', () => {
+      const result = 'ok';
+      expect(result).to.equal('ok');
+    });
+/*
+    it('call with params', () => {
+      const name = 'User';
+      const result = getHello(name);
+
+      expect(result).to.equal(`Hello dear ${name}`);
+    });
+    */
+  });
+});
